6429a2d196f311962c0c2b8510ab6388221a1e16
anandakshay20 <36408160+anandakshay20@users.noreply.github.com>
2018-02-19 17:45:40 +0530

    Add files via upload

---
+++ aks.3.py.py
@@ -0,0 +1,6 @@
+l = input("Input a letter of the alphabet: ")
+
+if l in ('a', 'e', 'i', 'o', 'u'):
+	print("The letter is a vowel." )
+else:
+	print("The letter is not a vowel." )
---
+++ aks.4.py.py
@@ -0,0 +1,5 @@
+sam = input("Enter a character: ")
+if((sam>='a' and sam<='z') or (sam>='A' and sam<='Z')):
+    print(sam, "is an Alphabet")
+else:
+    print(sam, "is not an Alphabet")
